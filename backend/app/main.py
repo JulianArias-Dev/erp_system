038ec0e25acd316ced2eeb169a90c4d5d3f8d070
JulianArias-Dev <davidariass0519@outlook.com>
2024-11-16 00:11:16 -0500

    endpoints for ingredients

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.controller import product_controller
+from app.controller import product_controller, ingredient_controller
 
 app = FastAPI()
 
@@ -15,3 +15,4 @@ app.add_middleware(
 )
 
 app.include_router(product_controller.router)
+app.include_router(ingredient_controller.router)
