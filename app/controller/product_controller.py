dbd6e8b7d73c0ed08271a758546881eee6bcd2b0
JulianArias-Dev <davidariass0519@outlook.com>
2024-11-21 08:18:59 -0500

    Modified endpoints to Json

--- app/controller/product_controller.py
+++ app/controller/product_controller.py
@@ -1,19 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 from app.config import get_db
-from app.domain.schemas.product_schema import ProductCreate, ProductResponse
+from app.domain.schemas.product_schema import ProductCreate
 from app.domain.services import product_service
 
 router = APIRouter(prefix="/products", tags=["Products"])
 
-@router.post("/", response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
 def create_product(product: ProductCreate, db: Session = Depends(get_db)):
     """
     Endpoint para crear un nuevo producto.
     """
-    return product_service.create_product(db, product)
-
-from fastapi.responses import JSONResponse
+    created_product = product_service.create_product(db, product)
+    return JSONResponse(
+        content={"message": "Product created successfully", "product": created_product},
+        status_code=status.HTTP_201_CREATED
+    )
 
 @router.get("/", response_model=dict)
 def list_products(db: Session = Depends(get_db)):
@@ -23,8 +26,7 @@ def list_products(db: Session = Depends(get_db)):
     products = product_service.list_products(db)
     return JSONResponse(content={"products": products})
 
-
-@router.get("/{code}", response_model=ProductResponse)
+@router.get("/{code}", response_model=dict)
 def get_product_by_code(code: str, db: Session = Depends(get_db)):
     """
     Endpoint para obtener un producto por su código.
@@ -32,31 +34,56 @@ def get_product_by_code(code: str, db: Session = Depends(get_db)):
     product = product_service.get_product_by_code(db, code)
     if not product:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Product not found"
         )
-    return product
+    return JSONResponse(content={"product": product})
 
-@router.put("/{id_product}", response_model=ProductResponse)
+@router.put("/{id_product}", response_model=dict)
 def update_product(id_product: int, new_product: ProductCreate, db: Session = Depends(get_db)):
     """
     Endpoint para actualizar un producto existente.
     """
-    product = product_service.update_product(db, id_product, new_product)
-    if not product:
+    updated_product = product_service.update_product(db, id_product, new_product)
+    if not updated_product:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Error updating product: Product not found"
         )
-    return product
+    return JSONResponse(
+        content={"message": "Product updated successfully", "product": updated_product}
+    )
 
-@router.delete("/{id_product}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id_product}", response_model=dict, status_code=status.HTTP_200_OK)
 def delete_product(id_product: int, db: Session = Depends(get_db)):
     """
     Endpoint para eliminar un producto por su ID.
     """
-    if not product_service.delete_product(db, id_product):
+    success = product_service.delete_product(db, id_product)
+    if not success:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Error deleting product: Product not found"
+        )
+    return JSONResponse(content={"message": "Product deleted successfully"})
+
+@router.put("/send-out/{product_code}:{sold_quantity}", response_model=dict)
+def send_out_product(product_code: str, sold_quantity: int, db: Session = Depends(get_db)):
+    """
+    Endpoint para simular el despacho de productos.
+    """
+    try:
+        sold_product = product_service.sold_product(product_code, sold_quantity, db)
+        return JSONResponse(
+            content={"message": "Sale successfully registered", "product": sold_product}
+        )
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An unexpected error occurred"
         )
--- app/domain/services/ingredient_service.py
+++ app/domain/services/ingredient_service.py
@@ -4,7 +4,15 @@ from app.domain.schemas.ingredient_schema import IngredientCreate
 
 def create_ingredient(db: Session, ingredient: IngredientCreate):
     """Crea un nuevo ingrediente."""
-    return ingredient_repository.create_ingrediet(db, ingredient)
+    ingredient = ingredient_repository.create_ingredient(db, ingredient)
+    return {
+        "id": ingredient.id,
+        "code": ingredient.code,
+        "name": ingredient.name,
+        "available_units": ingredient.available_units,
+        "max_capacity": ingredient.max_capacity,
+        "type" : ingredient.type,
+    }
 
 def list_ingredients(db: Session):
     """Devuelve una lista de todos los ingredientes en formato JSON."""
@@ -23,7 +31,17 @@ def list_ingredients(db: Session):
 
 def update_ingredient(db: Session, id_ingredient: int, new_ingredient: IngredientCreate):
     """Actualiza un ingrediente existente."""
-    return ingredient_repository.update_ingredient(db, id_ingredient, new_ingredient)
+    update_ingredient = ingredient_repository.update_ingredient(db, id_ingredient, new_ingredient)
+    if update_ingredient :
+        return {
+            "id": update_ingredient.id,
+            "name": update_ingredient.name,
+            "code": update_ingredient.code,
+            "available_units": update_ingredient.available_units,
+            "max_capacity": update_ingredient.max_capacity,
+            "type": update_ingredient.type,
+        }
+    return None
 
 def delete_ingredient(db: Session, id_ingredient: int):
     """Elimina un ingrediente por su ID."""
@@ -31,4 +49,14 @@ def delete_ingredient(db: Session, id_ingredient: int):
 
 def get_ingredient_by_code(db: Session, code: str):
     """Busca un ingrediente por su código."""
-    return ingredient_repository.get_ingredient_by_code(db, code)
+    ingredient = ingredient_repository.get_ingredient_by_code(db, code)
+    if ingredient :
+        return {
+             "id": ingredient.id,
+            "name": ingredient.name,
+            "code": ingredient.code,
+            "available_units": ingredient.available_units,
+            "max_capacity": ingredient.max_capacity,
+            "type": ingredient.type,
+        }
+    return None
